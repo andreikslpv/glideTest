96965f2ffdd5d7d7f4d4c79707c4865b2fe59683
andreikslpv <andrei.kslpv@yandex.ru>
2023-08-09 09:39:32 +0300

    1: отключена проверка SSL

--- app/src/main/java/com/andreikslpv/glidetest/MainActivity.kt
+++ app/src/main/java/com/andreikslpv/glidetest/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.andreikslpv.glidetest
 
-import android.net.Uri
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import java.io.File
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import okhttp3.OkHttpClient
+import java.io.InputStream
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,40 +23,31 @@ class MainActivity : AppCompatActivity() {
 
         val image = findViewById<ImageView>(R.id.itemImage)
 
-//        image.load("https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=172593&type=card")
+        val trustAllCertificates = arrayOf<TrustManager>(@SuppressLint("CustomX509TrustManager")
+        object : X509TrustManager {
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            @SuppressLint("TrustAllX509TrustManager")
+            override fun checkClientTrusted(certs: Array<X509Certificate>, authType: String) {}
+            @SuppressLint("TrustAllX509TrustManager")
+            override fun checkServerTrusted(certs: Array<X509Certificate>, authType: String) {}
+        })
 
-//        Glide.with(this)
-//            .load("https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=172593&type=card")
-//            .listener(object : RequestListener<Drawable> {
-//                override fun onLoadFailed(
-//                    e: GlideException?,
-//                    model: Any?,
-//                    target: Target<Drawable>?,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    println("AAA ${e?.rootCauses}")
-//                    return true
-//                }
-//
-//                override fun onResourceReady(
-//                    resource: Drawable?,
-//                    model: Any?,
-//                    target: Target<Drawable>?,
-//                    dataSource: DataSource?,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    println("AAA onResourceReady")
-//                    return true
-//                }
-//
-//            })
-//            .into(image)
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustAllCertificates, SecureRandom())
 
-        val photoUri = Uri.fromFile(File("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=172593&type=card"))
+        val okHttpClient = OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCertificates[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+
+        val factory = OkHttpUrlLoader.Factory(okHttpClient)
+
+        Glide.get(this).registry.replace(GlideUrl::class.java, InputStream::class.java, factory)
 
         Glide.with(this)
-            .load(photoUri)
-            .centerCrop()
+            .load("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=172593&type=card")
+            .fitCenter()
             .into(image)
+
     }
 }
